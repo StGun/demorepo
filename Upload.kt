7a6c29b47ddd5488887a27fcac5cd6b234d0f4b6
Natiq Haciyev <100693634+natiqhaciyef@users.noreply.github.com>
2022-11-13 20:02:36 +0400

    Update Upload.kt

--- Upload.kt
+++ Upload.kt
@@ -12,22 +12,7 @@ class CategoriesAdapter(var mContext: Context, var list: List<Category>) : Recyc
 
     inner class CategoryHolder(var binding: RecyclerCategoriesBinding): RecyclerView.ViewHolder(binding.root)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryHolder {
-        val binding = RecyclerCategoriesBinding.inflate(LayoutInflater.from(mContext), parent, false)
-        return CategoryHolder(binding)
-    }
 
-    override fun onBindViewHolder(holder: CategoryHolder, position: Int) {
-        val itemView = holder.binding
-        val category = list[position]
-
-        itemView.recyclerCategoriesNameText.text = category.name
-
-        if (category.image.isNullOrEmpty())
-            itemView.recyclerCategoriesImage.visibility = View.GONE
-        else
-            itemView.recyclerCategoriesImage.setImageResource(mContext.resources.getIdentifier(category.image, "drawable",mContext.packageName))
-    }
 
     override fun getItemCount(): Int {
         return list.size
